6ffc244c494141a1f78527b4864d818f48af76f9
Angel Jesus <230592@utxicotepec.edu.mx>
2024-12-18 15:15:22 -0600

    v1
    agregacion de primeros templates, logi,register,forgetPassword ademas de routing entre estos para mejor experiencia de usuario asi mismo implementando colores más agradables

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,21 @@
 //funcionalidad de controlador
 const formLogin = (req, res) => {
     res.render('auth/login', {
-        authenticated: false
+        authenticated: false,
+        pagina: 'Iniciar Sesion'
     })
 }
 
 const formRegister = (req, res) => {
     res.render('auth/register', {
-        
+        pagina: 'Crear Cuenta'
+    })
+}
+
+
+const forgetPassword = (req, res) => {
+    res.render('auth/forget-password', {
+        pagina: 'Recupera tu acceso a Bienes Raices'
     })
 }
 
@@ -19,5 +27,6 @@ const formRegister = (req, res) => {
 //export nombrado o el expor default solo uno por archivo y teniendo uno nombrado se pueden tener varias funciones con este, en default puede tener el nombre x
 export{
     formLogin,
-    formRegister
+    formRegister,
+    forgetPassword
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { formLogin, formRegister } from "../controllers/userController.js";
+import { forgetPassword, formLogin, formRegister } from "../controllers/userController.js";
 
 const router = express.Router();
 //visitamos la pagina de login y pedimos o renderizamos el la funcion de formulario login desde nuestro controller
 router.get('/login', formLogin);
 
 router.get('/register', formRegister)
+
+router.get('/forget-Password', forgetPassword)
 
 
  
